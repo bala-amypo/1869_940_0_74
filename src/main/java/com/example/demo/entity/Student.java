578420f0c29bbd80ee495e1cae5e66156906d75c
bala-amypo <bala@amypo.in>
2025-12-17 06:40:11 +0000

    User commit

--- src/main/java/com/example/demo/entity/Student.java
+++ src/main/java/com/example/demo/entity/Student.java
@@ -1,17 +1,25 @@
-package com.example.aiml.entity;
+package com.example.demo.entity;
 
-
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+@Entity
+//@Table(name="StudentTable")
 public class Student {
-    private int id ;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id ;
     private String name;
     private String email;
     
     
-    public void setId(int id){
+    public void setId(Long id){
         this.id=id;
 
     }
-    public int getId() {
+    public Long getId() {
         return id;
         
     }
@@ -28,7 +36,7 @@ public class Student {
         this.email = email;
     }
     
-    public Student(int id, String name, String email) {
+    public Student(Long id, String name, String email) {
         this.id = id;
         this.name = name;
         this.email = email;
