23f23b26f87d59def3d2a2575aeeb6cc7e4fdb63
bala-amypo <bala@amypo.in>
2025-12-19 06:33:08 +0000

    User commit

--- src/main/java/com/service/StudentService.java
+++ src/main/java/com/service/StudentService.java
@@ -1,9 +1,38 @@
-package com.example.demo.service;
+package com.example.demo.Serviceimpl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.example.demo.entity.Student;
+import com.example.demo.repository.StudentRepository;
+import com.example.demo.service.StudentService;
+import com.example.demo.exception.ResourceNotFoundException;
 
-public interface StudentService {
+@Service
+public class StudentServiceImple implements StudentService {
+    @Autowired
+    StudentRepository studentRepository;
 
-    Student saveStudent(Student student);
-    Student getStudentById(Long id);
+    public Student saveStudent(Student student){
+        return studentRepository.save(student);
+    }
+    
+    public Student getStudentById(Long id){
+        return studentRepository.findById(id)
+        .orElseThrow(()-> new ResourceNotFoundException("Student not found"));
+    }
+    public Student updatedata(Long id,Student student){
+        Student exits=getStudentById(id);
+        exits.setName(student.getName());
+        exits.setEmail(student.getEmail());
+        return studentRepository.save(exits);
+        // .orElseThrow(()-> new ResourceNotFoundException("Student not found"));
+    }
+
+    public Student deletdata(Long id){
+        Student student=getStudentById(id);
+        studentRepository.delete(student);
+        return student;
+    }
+
 }
